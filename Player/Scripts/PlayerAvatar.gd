fbf1de3f09969c16189fe457062fc70303e640d2
Shantel Bosmer <sbosmer@oa-l3.21-school.ru>
2019-11-16 14:09:30 +0300

    still project settings

--- Player/Scripts/PlayerAvatar.gd
+++ Player/Scripts/PlayerAvatar.gd
@@ -11,19 +11,11 @@ func _process(delta):
 		velocity = Vector2.ZERO
 	if Input.is_action_pressed("player_right"):
 		velocity.x += 1
-<<<<<<< HEAD
-	if Input.is_action_pressed("player_left"):
+	elif Input.is_action_pressed("player_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("player_up"):
+	elif Input.is_action_pressed("player_up"):
 		velocity.y -= 1
-	if Input.is_action_pressed("player_down"):
-=======
-	elif Input.is_action_pressed("left"):
-		velocity.x -= 1
-	elif Input.is_action_pressed("up"):
-		velocity.y -= 1
-	elif Input.is_action_pressed("down"):
->>>>>>> 91689008b0d96f5caf745b855aca6b5c7466165c
+	elif Input.is_action_pressed("player_down"):
 		velocity.y += 1
 	velocity *= 1.5
 
