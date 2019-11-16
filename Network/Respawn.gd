197621771fb3c15f559d308192d9fe28c06dcb4d
StarNuik <starnuik@gmail.com>
2019-11-17 00:09:02 +0300

    spawner added

---
+++ Network/Respawn.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var spawn_points = Array()
+var current_point = 0
+
+func init():
+	randomize()
+	var points = get_node("/root/Game/Map/Spawn Points")
+	for p in points.get_children():
+		spawn_points.append(p.position)
+		p.queue_free()
+	current_point = randi() % len(spawn_points)
+
+func player(pid):
+	var player_node = get_node("/root/Game/" + str(pid))
+	var avatar = player_node.get_node("PlayerAvatar")
+	var pos = spawn_points[current_point]
+	avatar.sync_pos(pos)
+	avatar.rpc("sync_pos", pos)
+	current_point += 1
+	if current_point == len(spawn_points):
+		current_point = 0
+	pass
