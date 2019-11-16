8407eb8a4df55a7e26e4aad236c26ec1fe56fba5
Maksim <prokushevmaksim@gmail.com>
2019-11-16 16:17:45 +0300

    added player movement

--- Player/Scripts/PlayerAvatar.gd
+++ Player/Scripts/PlayerAvatar.gd
@@ -1,24 +1,53 @@
 extends KinematicBody2D
 
 onready var velocity = Vector2(0.0, 0.0)
+var is_on_collide = false
+var check 
+var speed = 500
+var one_tap = true
 
 func _ready():
 	
 	pass # Replace with function body.
 
 func _process(delta):
-	if (move_and_collide(velocity * delta)):
+	if one_tap == true:
+		_check_input(delta)
+	velocity = velocity.normalized()
+	if (move_and_collide(velocity * speed * delta)):
+		one_tap = true
 		velocity = Vector2.ZERO
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-	elif Input.is_action_pressed("left"):
-		velocity.x -= 1
-	elif Input.is_action_pressed("up"):
-		velocity.y -= 1
-	elif Input.is_action_pressed("down"):
-		velocity.y += 1
-	velocity *= 1.5
+
+func _check_input(delta : float):
+	if Input.is_action_just_pressed("right"):
+		velocity.x = 1
+		if ((_check_direction(delta))):
+			one_tap = true
+			return
+	elif Input.is_action_just_pressed("left"):
+		velocity.x = -1
+		if ((_check_direction(delta))):
+			one_tap = true
+			return
+	elif Input.is_action_just_pressed("up"):
+		velocity.y = -1
+		if ((_check_direction(delta))):
+			one_tap = true
+			return
+	elif Input.is_action_just_pressed("down"):
+		velocity.y = 1
+		if ((_check_direction(delta))):
+			one_tap = true
+			return
+	if (velocity != Vector2.ZERO):
+		one_tap = false
+
+func _check_direction(delta : float) -> bool :
+	check = test_move(transform, (velocity * delta))
+	if (!check):
+		return true
+	return false
 
 func _physics_process(delta):
-		
+
 	pass
