17c72f85ccc96452fc778faeec835fd7b07887dc
Maksim <prokushevmaksim@gmail.com>
2019-11-17 00:55:07 +0300

    player death added

--- Player/Scripts/PlayerAvatar.gd
+++ Player/Scripts/PlayerAvatar.gd
@@ -1,13 +1,15 @@
 extends KinematicBody2D
 
 onready var velocity = Vector2.ZERO
+onready var tmp_velocity = velocity
 const speed = 500
 var one_tap = true
 var is_controlled = false
 
 func _ready():
-	$AreaTakeDamage.connect("area_entered", self, "damage")
-	$AreaAttack.connect("area_entered", self, "attack")
+	$AreaAttack.connect("area_entered", self, "area_collision")
+	$AreaAttack.connect("body_entered", self, "collision")
+#	$AreaTakeDamage.connect("area_entered", self, "damage")
 	pass
 
 func _process(delta):
@@ -24,6 +26,7 @@ func _process(delta):
 		input.x = -1
 #	if _possible_to_move(delta, input):
 #		return
+#	print(velocity)
 	if velocity == Vector2.ZERO and input != Vector2.ZERO:
 		rpc("change_velocity", input)
 		rpc("sync_pos", position)
@@ -32,10 +35,13 @@ func _process(delta):
 func _physics_process(delta : float):
 	var col = move_and_collide(velocity * delta * speed)
 	if col:
+		if tmp_velocity != Vector2.ZERO:
+			_turn_around()
 		change_velocity(Vector2.ZERO)
 
 remotesync func change_velocity(input: Vector2):
 	velocity = input
+	tmp_velocity = velocity
 
 func _possible_to_move(delta : float, input : Vector2):
 	var check_move = test_move(transform, input * delta)
@@ -54,13 +60,37 @@ remotesync func _change_body_direction():
 	elif (velocity.y == 0):
 		set_rotation_degrees(velocity.x * 90)
 
-func attack(body):
-	if (body != self and body.get_node("..").has_method("damage")):
-		print("Attack!")
-		body.get_node("..").damage(1)
+func _turn_around():
+	set_rotation_degrees(rotation_degrees + 180)
+
+
+func area_collision(area: Area2D):
+	var mb_player = area.get_node("..")
+	if mb_player != self and mb_player.has_method("damage"):
+		velocity = -velocity
 	pass
 
-func damage(amount : int):
+
+func collision(body: PhysicsBody2D):
+	if body and body != self and body.has_method("damage"):
+		print("PEPEGA V EBALO")
+		body.rpc("damage", 999)
+	pass
+
+
+#func attack(body):
+#	print("pepega 1")
+#	if body != self and body.get_node("..").has_method("damage"):
+#		if body.name == "AreaAttack":
+##			print(velocity)
+#			velocity = -velocity
+#			print("Check")
+##			print(velocity)
+#			return
+#		body.get_node("..").damage(1)
+#
+remotesync func damage(amount : int):
+	print("PEPEGUS MAXIMUS")
 	queue_free()
 	print("Taken ", amount, " damage!")
 
