83ac03757b86a45ccf4541a944bf928fa0a629cb
sleonard <sleonard@student.42.fr>
2019-11-17 12:15:35 +0300

    Done teleports cooldowns by players, done periodic spikes, done trap after player step on it

--- Enviroment/PortalSystem.gd
+++ Enviroment/PortalSystem.gd
@@ -3,18 +3,44 @@ extends Node2D
 export(float) var teleport_cooldown = 2
 export(float) var delay_before_teleport = 0
 
-var timer_started : bool = false
+var players_list = {}
+
+var timer_resource = preload("res://Enviroment/CustomTeleportTimer.tscn")
 
 func _ready() -> void:
 	pass
 	
 
-func start_timer():
-	$Delay.start(teleport_cooldown)
-	timer_started = true
+func start_timer(player_name : String):
+	var timer_node = find_timer(player_name)
+	timer_node.start(teleport_cooldown)
 	
 
-func _on_Delay_timeout() -> void:
-	timer_started = false
+func find_timer(player_name : String):
+	for timer in get_children():
+		if timer is Timer and timer.player_name == player_name:
+			return timer
+	var new_timer = timer_resource.instance()
+	add_child(new_timer)
+	new_timer.init(player_name)
+#	new_timer.connect("timeout", self, "_on_Delay_timeout")
+	return new_timer
+		
+		
+func stop_timer(player_name : String):
+	var timer = find_timer(player_name)
+	timer.stop()
+	
+
+func teleport_on_cooldown(player_name : String) -> bool:
+	var timer = find_timer(player_name)
+#	print("time left: ", timer.get_time_left())
+	if timer.get_time_left() <= 0:
+		return false
+	return true
+
+
+#func _on_Delay_timeout() -> void: #todo pass string
+#	print("TIMEOUT")
 
 	
--- Enviroment/Teleport.gd
+++ Enviroment/Teleport.gd
@@ -21,15 +21,15 @@ func get_another_teleport_pos():
 						
 
 func _teleport(body : PhysicsBody2D):
-#	if body.is_type("Player") todo ?
 	player_on_teleport = true
-	if !get_parent().timer_started:
+	if !get_parent().teleport_on_cooldown(body.get_parent().name):
 		yield(get_tree().create_timer(delay_before_teleport), "timeout")
 		if player_on_teleport:
 			body.global_position = another_portal_pos
-			get_parent().start_timer()
+			get_parent().start_timer(body.get_parent().name)
+	
 		
 func _player_exited(body : PhysicsBody2D):
 	player_on_teleport = false
-	get_parent().timer_started = false
+#	get_parent().stop_timer(body.get_parent().name) # можно раскомментить чтобы таймер перезагружался каждый раз при выходе перса из Area2D
 		
