3d5370249c9feae45a7081bd04d3f0eb5f35e806
sleonard <sleonard@student.42.fr>
2019-11-17 21:59:21 +0300

    fixed spikes

--- Enviroment/Spikes.gd
+++ Enviroment/Spikes.gd
@@ -3,6 +3,8 @@ extends Area2D
 var spikes_duration : float
 var is_active : bool = true
 
+var current_body : PhysicsBody2D = null
+
 func _ready() -> void:
 	spikes_duration = $AnimationPlayer.get_animation("default").length / $AnimationPlayer.playback_speed
 	$Timer.start(spikes_duration)
@@ -10,8 +12,18 @@ func _ready() -> void:
 	
 
 func _on_Spikes_body_entered(body: PhysicsBody2D) -> void:
+	current_body = body
+
+
+func _physics_process(delta: float) -> void:
+	if is_active and current_body:
+		deal_damage(current_body)
+	pass
+
+
+func deal_damage(body : PhysicsBody2D):
 	if is_active and body.has_method("damage"):
-		body.damage(21, 0)
+		body.damage(42, 0)
 
 
 func _on_Timer_timeout() -> void:
@@ -22,3 +34,9 @@ func _on_Timer_timeout() -> void:
 	else:
 		$AnimationPlayer.play("default")
 		is_active = true
+		
+
+
+
+func _on_Spikes_body_exited(body: PhysicsBody2D) -> void:
+	current_body = null
